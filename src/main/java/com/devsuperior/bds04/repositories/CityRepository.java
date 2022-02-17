9f4ae64be125cba6c1edabe6db62e92a4c7f1c73
MarceloPN <marcelonogueira072@gmail.com>
2022-02-17 17:31:46 -0300

    Infraestrutura de segurança

---
+++ src/main/java/com/devsuperior/bds04/repositories/CityRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.bds04.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.User;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+
+	
+
+
+
+}
